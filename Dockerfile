26b9fe9d11ef1e2ef711a3d61c7e7719023adb82
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2017-06-17 14:30:28 +0200

    Deploy to gh-pages

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node
+
+RUN git config --global user.email "servant@userfeeds.io"
+RUN git config --global user.name "Userfeeds Servant"
+
+WORKDIR /
+
+RUN npm install yarn
+COPY . /apps
+
+WORKDIR /apps/
+RUN /apps/release.sh
---
+++ release.sh
@@ -0,0 +1,6 @@
+find . -maxdepth 2 -type d -regex '\./[a-z]+.*' -exec \
+    bash -c "echo '{}' && cd '{}' && [ -f package.json ] && yarn install && yarn build && mkdir -p /apps/release/{} && mv build /apps/release/{}" \;
+
+git add /apps/release
+git commit -m "Release"
+git subtree push --prefix=release git@github.com:Userfeeds/Apps.git gh-pages
