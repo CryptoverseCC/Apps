5a76ed378eaae4b8cb46f86a01b97adf8f608d9f
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2017-06-17 15:00:17 +0200

    Github.com fingerprint issue #2: https://community.codeship.com/t/the-authenticity-of-host-github-com-192-30-252-129-cant-be-established/257

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 
+echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
+chmod -R 700 $HOME/.ssh
+
 find . -maxdepth 2 -type d -regex '\./[a-z]+.*' -exec \
     bash -c "echo '{}' && cd '{}' && [ -f package.json ] && yarn install && yarn build && mkdir -p /apps/release/{} && mv build /apps/release/{}" \;
 
