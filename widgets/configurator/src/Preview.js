8ad570f2d8a2774cd10bd0c85f6bfa34e71d96e6
Jakub Stasiak <kubafr2@gmail.com>
2017-06-18 13:21:59 +0200

    Removed api-key

--- widgets/configurator/src/Preview.js
+++ widgets/configurator/src/Preview.js
@@ -10,7 +10,6 @@ const Preview = ({ widgetSettings }) => {
         size={widgetSettings.size}
         algorithm={widgetSettings.algorithm}
         context={`${widgetSettings.network}:${widgetSettings.userfeedsId}`}
-        api-key="59049c8fdfed920001508e2aafdcb00bdd4c4c7d61ca02ff47080fe3"
       >
       </userfeeds-ad>
     </div>
