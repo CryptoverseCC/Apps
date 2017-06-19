1c65b538849f1ec707f828d32a43df1967f92386
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2017-06-19 11:14:13 +0200

    Finally working deploy configuration

--- release.sh
+++ release.sh
@@ -3,8 +3,11 @@
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 
-echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
+echo -e "$PRIVATE_SSH_KEY" >> $HOME/.ssh/id_rsa
+
 chmod -R 700 $HOME/.ssh
+
+ssh -T git@github.com
 
 git status
 
@@ -12,4 +15,5 @@ ls -l /apps/release
 
 git add /apps/release
 git commit -m "Release"
-git subtree push -d --prefix=release git@github.com:Userfeeds/Apps.git gh-pages
+# http://clontz.org/blog/2014/05/08/git-subtree-push-for-deployment/
+git push origin `git subtree split --prefix release master`:gh-pages --force
