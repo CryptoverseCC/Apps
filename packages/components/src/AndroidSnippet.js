c0c6ba87671bdf69160506fc86481877ffccac6e
Maciej Górski <maciek.gorski@gmail.com>
2017-06-26 15:34:52 +0200

    Add info about widget for Android.

---
+++ packages/components/src/AndroidSnippet.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Highlight from 'react-highlight';
+
+import 'highlight.js/styles/androidstudio.css';
+
+export default class AndroidSnippet extends Component {
+
+  render() {
+    const { widgetSettings } = this.props;
+    return (
+      <div>
+        <Highlight className="xml">
+          {`
+  <io.userfeeds.widget.LinksViewPager
+      xmlns:userfeeds="http://schemas.android.com/apk/res-auto"
+      android:layout_width="match_parent"
+      android:layout_height="wrap_content"
+      userfeeds:context="${widgetSettings.network}:${widgetSettings.userfeedsId}"
+      userfeeds:algorithm="${widgetSettings.algorithm}"/>
+          `}
+        </Highlight>
+        <Highlight className="groovy">
+          {`
+  dependencies {
+      // find latest version here:
+      // http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22io.userfeeds.widget%22
+      compile 'io.userfeeds.widget:core:(latest version goes here)'
+  }
+          `}
+        </Highlight>
+      </div>
+    );
+  }
+}
