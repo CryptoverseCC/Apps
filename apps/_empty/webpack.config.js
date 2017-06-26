ab1bbaf2d225af7b81cc7dcce2f9018ac23400c3
Jakub Stasiak <kubafr2@gmail.com>
2017-06-26 12:01:14 +0200

    Empty project - boilerplate (#6)

---
+++ apps/_empty/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const IS_DEV = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: IS_DEV ? '' : '/apps/links/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+    }, {
+      test: /\.(css|scss)$/,
+      exclude: /node_modules\/(?!@userfeeds)/,
+      use: [
+        { loader: 'style-loader' },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            sourceMap: true,
+            camelCase: true,
+            localIdentName: '[name]__[local]--[hash:base64:5]',
+          },
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+            plugins: () => {
+              autoprefixer({ browsers: ['last 2 versions'] });
+            },
+          },
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    }, {
+      test: /\.(css|scss)$/,
+      include: /node_modules\/(?!@userfeeds)/,
+      use: [
+        { loader: 'style-loader' },
+        {
+          loader: 'css-loader',
+          options: { importLoaders: 1, sourceMap: true },
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+            plugins: () => {
+              autoprefixer({ browsers: ['last 2 versions'] });
+            },
+          },
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+};
