99e9f2cf3dec17bfa6af741cecef174aee2fff77
Maciej Górski <maciek.gorski@gmail.com>
2017-07-03 10:51:55 +0200

    Replace with template string.

--- packages/JS-Widgets/src/utils/openUserfeedsUrl.js
+++ packages/JS-Widgets/src/utils/openUserfeedsUrl.js
@@ -1,10 +1,10 @@
 
 export const openUserfeedsUrl = (path, { context, algorithm, whitelist, publisherNote, linkId }) => {
   const baseUrl = 'https://userfeeds.io/';
-  const contextQP = '?context=' + encodeURIComponent(context);
-  const algorithmQP = '&algorithm=' + encodeURIComponent(algorithm);
-  const whitelistQP = whitelist ? '&whitelist=' + encodeURIComponent(whitelist) : '';
-  const publisherNoteQP = publisherNote ? '&publisherNote=' + encodeURIComponent(publisherNote) : '';
-  const linkIdQP = linkId ? '&linkId=' + encodeURIComponent(linkId) : '';
+  const contextQP = `?context=${encodeURIComponent(context)}`;
+  const algorithmQP = `&algorithm=${encodeURIComponent(algorithm)}`;
+  const whitelistQP = whitelist ? `&whitelist=${encodeURIComponent(whitelist)}` : '';
+  const publisherNoteQP = publisherNote ? `&publisherNote=${encodeURIComponent(publisherNote)}` : '';
+  const linkIdQP = linkId ? `&linkId=${encodeURIComponent(linkId)}` : '';
   window.open(baseUrl + path + contextQP + algorithmQP + whitelistQP + publisherNoteQP + linkIdQP, '_blank');
 };
