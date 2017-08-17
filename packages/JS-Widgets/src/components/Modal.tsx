948698f2fc8a03eafd594dd1ee0a1362976ce8ea
Jakub Stasiak <kubafr2@gmail.com>
2017-08-17 14:11:30 +0200

    Override wheel and mousewheel event listeners when modal is visible

--- packages/JS-Widgets/src/components/Modal.tsx
+++ packages/JS-Widgets/src/components/Modal.tsx
@@ -9,6 +9,16 @@ interface IModalProps {
 
 export default class Modal extends Component<IModalProps, {}> {
 
+  componentDidMount() {
+    document.addEventListener('wheel', this._consumeEvent);
+    document.addEventListener('mousewheel', this._consumeEvent);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('wheel', this._consumeEvent);
+    document.removeEventListener('mousewheel', this._consumeEvent);
+  }
+
   render() {
     const { isOpen, children } = this.props;
     if (!isOpen) {
@@ -17,7 +27,7 @@ export default class Modal extends Component<IModalProps, {}> {
 
     return (
       <div class={style.self} onClick={this._onOverlayClick}>
-        <div class={style.content} onClick={this._onContentClick}>
+        <div class={style.content} onClick={this._consumeEvent}>
           {children}
         </div>
       </div>
@@ -30,7 +40,7 @@ export default class Modal extends Component<IModalProps, {}> {
     }
   }
 
-  _onContentClick = (event) => {
-    event.stopPropagation();
+  _consumeEvent = (event: MouseEvent) => {
+    event.stopImmediatePropagation();
   }
 }
