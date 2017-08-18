df1768100b045b3e36e4cd343e83bd3f2d50f866
Jakub Stasiak <kubafr2@gmail.com>
2017-08-18 10:02:31 +0200

    Improve publish script

--- packages/JS-Widgets/publish.sh
+++ packages/JS-Widgets/publish.sh
@@ -2,5 +2,14 @@
 
 echo "~~~~~Publishing version: $CI_BRANCH~~~~~"
 echo $NPMRC > ~/.npmrc
-npm publish
-curl http://purge.jsdelivr.net/npm/@userfeeds/widgets@latest
+
+IS_STABLE=`git tag --points-at HEAD | grep -E "^stable$"`
+if [ -n "$IS_STABLE" ]; then
+  echo "IS STABLE"
+  npm publish --tag stable
+  curl http://purge.jsdelivr.net/npm/@userfeeds/widgets@stable
+else
+  echo "NOT STABLE"
+  npm publish
+  curl http://purge.jsdelivr.net/npm/@userfeeds/widgets@latest
+fi
