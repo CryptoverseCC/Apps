65846ceb57b93832dd9c54f458e847204a9e4fa8
Jakub Stasiak <kubafr2@gmail.com>
2017-08-18 11:15:29 +0200

    publishing latest version and tagging when needed

--- packages/JS-Widgets/publish.sh
+++ packages/JS-Widgets/publish.sh
@@ -6,10 +6,13 @@ echo $NPMRC > ~/.npmrc
 IS_STABLE=`git tag --points-at HEAD | grep -E "^stable$"`
 if [ -n "$IS_STABLE" ]; then
   echo "IS STABLE"
-  npm publish --tag stable
+  NPM_VERSION=`node -e "console.log(require('./package.json').version);"`
+  npm publish
+  npm dist-tag add "@userfeeds/widgets@$NPM_VERSION" stable
   curl http://purge.jsdelivr.net/npm/@userfeeds/widgets@stable
 else
   echo "NOT STABLE"
   npm publish
-  curl http://purge.jsdelivr.net/npm/@userfeeds/widgets@latest
 fi
+
+curl http://purge.jsdelivr.net/npm/@userfeeds/widgets@latest
