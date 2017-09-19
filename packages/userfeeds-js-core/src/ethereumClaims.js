fcbbd52ea8cd029a13d0a5a548ba6ee69487c708
Maciej Górski <maciek.gorski@gmail.com>
2017-09-19 11:04:08 +0200

    Update sendClaim functions to call new contracts.

--- packages/userfeeds-js-core/src/ethereumClaims.js
+++ packages/userfeeds-js-core/src/ethereumClaims.js
@@ -1,64 +1,72 @@
 const { getCurrentNetworkName, getAccounts } = require('./utils');
 const {
-  payableAbi,
-  notpayableAbi,
-  getContractAddress,
+  getContractWithoutValueTransfer,
+  getContractValueTransfer,
+  getContractTokenTransfer,
 } = require('./utils/contract');
 
-async function sendPayableClaim(web3Instance, address, claim, value) {
+async function sendClaimWithoutValueTransfer(web3Instance, claim) {
   const networkName = await getCurrentNetworkName(web3Instance);
   const [from] = await getAccounts(web3Instance);
 
-  const contract = web3Instance.eth.contract(payableAbi)
-    .at(getContractAddress(networkName, true));
+  const contract = getContractWithoutValueTransfer(web3Instance, networkName);
+
+  return new Promise((resolve, reject) => {
+    contract.post(
+      JSON.stringify(claim),
+      { from },
+      getResolveOrRejectOnErrorFunc(resolve, reject),
+    );
+  });
+}
+
+async function sendClaimValueTransfer(web3Instance, address, value, claim) {
+  const networkName = await getCurrentNetworkName(web3Instance);
+  const [from] = await getAccounts(web3Instance);
+
+  const contract = getContractValueTransfer(web3Instance, networkName);
 
   return new Promise((resolve, reject) => {
     contract.post(
       address,
       JSON.stringify(claim),
       { from, value: web3Instance.toWei(value, 'ether') },
-      (errror, result) => {
-        if (errror) {
-          return reject(errror);
-        }
-        return resolve(result);
-      },
+      getResolveOrRejectOnErrorFunc(resolve, reject),
     );
   });
 }
 
-async function sendNotpayableClaim(web3Instance, address, claim) {
-  const networkName = await getCurrentNetworkName(web3Instance)
+async function sendClaimTokenTransfer(web3Instance, address, token, value, claim) {
+  const networkName = await getCurrentNetworkName(web3Instance);
   const [from] = await getAccounts(web3Instance);
 
-  const contract = web3Instance.eth.contract(notpayableAbi)
-    .at(getContractAddress(networkName, false));
+  const contract = getContractTokenTransfer(web3Instance, networkName);
 
   return new Promise((resolve, reject) => {
     contract.post(
+      address,
+      token,
+      value,
       JSON.stringify(claim),
       { from },
-      (errror, result) => {
-        if (errror) {
-          return reject(errror);
-        }
-        return resolve(result);
-      },
+      getResolveOrRejectOnErrorFunc(resolve, reject),
     );
   });
 }
 
-function sendClaim(web3Instance, address, claim, value) {
-  const payable = value !== undefined;
-
-  return payable
-    ? sendPayableClaim(web3Instance, address, claim, value)
-    : sendNotpayableClaim(web3Instance, address, claim);
+function getResolveOrRejectOnErrorFunc(resolve, reject) {
+  return function(error, result) {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(result);
+    }
+  };
 }
 
 module.exports = {
-  sendClaim,
-  sendPayableClaim,
-  sendNotpayableClaim,
+  sendClaimWithoutValueTransfer,
+  sendClaimValueTransfer,
+  sendClaimTokenTransfer,
 };
 
--- packages/userfeeds-js-core/src/utils/contract.js
+++ packages/userfeeds-js-core/src/utils/contract.js
@@ -1,71 +1,90 @@
-
-const payableContractAddressMapping = {
-  ropsten: '0xa845c686a696c3d33988917c387d8ab939c66226',
-  rinkeby: '0x0a48ac8263d9d79768d10cf9d7e82a19c49f0002',
+const withoutValueTransferContractAddressMapping = {
+  ethereum: '0xFd74f0ce337fC692B8c124c094c1386A14ec7901',
+  rinkeby: '0xC5De286677AC4f371dc791022218b1c13B72DbBd',
+  ropsten: '0x6f32a6F579CFEed1FFfDc562231C957ECC894001',
+  kovan: '0x139d658eD55b78e783DbE9bD4eb8F2b977b24153'
 };
 
-const notpayableContractAddressMapping = {
-  ropsten: '0x5c3fe6b94b57c1e294000403340f12f083e71b83',
-  rinkeby: '0x09dcdf34e0c28b106fdfe51009cb71ae92bf8bbc',
+const valueTransferContractAddressMapping = {
+  ethereum: '0x70B610F7072E742d4278eC55C02426Dbaaee388C',
+  rinkeby: '0x00034B8397d9400117b4298548EAa59267953F8c',
+  ropsten: '0x37C1CA7996CDdAaa31e13AA3eEE0C89Ee4f665B5',
+  kovan: '0xc666c75C2bBA9AD8Df402138cE32265ac0EC7aaC'
 };
 
-function getContractAddress(networkName, payable = true) {
-  let contract;
-  if (payable) {
-    contract = payableContractAddressMapping[networkName];
-  } else {
-    contract = notpayableContractAddressMapping[networkName];
-  }
+const tokenTransferContractAddressMapping = {
+  ethereum: '0xfF8A1BA752fE5df494B02D77525EC6Fa76cecb93',
+  rinkeby: '0xBd2A0FF74dE98cFDDe4653c610E0E473137534fB',
+  ropsten: '0x54b4372fA0bd76664B48625f0e8c899Ff19DFc39',
+  kovan: '0xd6Ede7F43882B100C6311a9dF801088eA91cEb64'
+};
 
-  if (!contract) {
+function getContractWithoutValueTransfer(web3Instance, networkName) {
+  let contractAddress = withoutValueTransferContractAddressMapping[networkName];
+  if (!contractAddress) {
     throw new Error('Contract is not available');
   }
-
+  let contract = web3Instance.eth.contract(withoutValueTransferAbi).at(contractAddress);
   return contract;
 }
 
-const payableAbi = [{
+const withoutValueTransferAbi = [{
   constant: false,
   inputs: [
-    { name: 'userfeed', type: 'address' },
-    { name: 'data', type: 'string' },
-  ],
-  name: 'post',
-  outputs: [],
-  payable: true,
-  type: 'function',
-}, {
-  anonymous: false,
-  inputs: [
-    { name: 'sender', type: 'address', indexed: false },
-    { name: 'userfeed', type: 'address', indexed: false },
-    { name: 'data', type: 'string', indexed: false },
-  ],
-  name: 'Claim',
-  type: 'event',
-}];
-
-const notpayableAbi = [{
-  constant: false,
-  inputs: [
-    { name: 'data', type: 'string' },
+    { name: 'data', type: 'string' }
   ],
   name: 'post',
   outputs: [],
   payable: false,
-  type: 'function',
-}, {
-  anonymous: false,
+  type: 'function'
+}];
+
+function getContractValueTransfer(web3Instance, networkName) {
+  let contractAddress = valueTransferContractAddressMapping[networkName];
+  if (!contractAddress) {
+    throw new Error('Contract is not available');
+  }
+  let contract = web3Instance.eth.contract(valueTransferAbi).at(contractAddress);
+  return contract;
+}
+
+const valueTransferAbi = [{
+  constant: false,
   inputs: [
-    { name: 'sender', type: 'address', indexed: false },
-    { name: 'data', type: 'string', indexed: false },
+    { name: 'userfeed', type: 'address' },
+    { name: 'data', type: 'string' }
   ],
-  name: 'Claim',
-  type: 'event'
+  name: 'post',
+  outputs: [],
+  payable: true,
+  type: 'function'
+}];
+
+function getContractTokenTransfer(web3Instance, networkName) {
+  let contractAddress = tokenTransferContractAddressMapping[networkName];
+  if (!contractAddress) {
+    throw new Error('Contract is not available');
+  }
+  let contract = web3Instance.eth.contract(tokenTransferAbi).at(contractAddress);
+  return contract;
+}
+
+const tokenTransferAbi = [{
+  constant: false,
+  inputs: [
+    { name: 'userfeed', type: 'address' },
+    { name: 'token', type: 'address' },
+    { name: 'value', type: 'uint256' },
+    { name: 'data', type: 'string' }
+  ],
+  name: 'post',
+  outputs: [],
+  payable: false,
+  type: 'function'
 }];
 
 module.exports = {
-  payableAbi,
-  notpayableAbi,
-  getContractAddress,
+  getContractWithoutValueTransfer,
+  getContractValueTransfer,
+  getContractTokenTransfer,
 };
