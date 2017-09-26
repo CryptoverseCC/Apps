fd2214ebd9e087f1ea9f4f5713512450283e1527
Maciej Górski <maciek.gorski@gmail.com>
2017-09-26 15:18:23 +0200

    Move erc20 export to ethereum.

--- packages/userfeeds-js-core/index.js
+++ packages/userfeeds-js-core/index.js
@@ -2,7 +2,7 @@
 module.exports = {
   ethereum: {
     claims: require('./src/ethereumClaims'),
+    erc20: require('./src/erc20'),
   },
   utils: require('./src/utils'),
-  erc20: require('./src/erc20'),
 };
