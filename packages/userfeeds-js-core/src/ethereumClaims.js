b28a625c4b84af8da745dd15a8cc2783cc158f24
Maciej Górski <maciek.gorski@gmail.com>
2017-09-29 15:04:14 +0200

    Add allowanceUserfeedsContractTokenTransfer to module exports.

--- packages/userfeeds-js-core/src/ethereumClaims.js
+++ packages/userfeeds-js-core/src/ethereumClaims.js
@@ -82,6 +82,7 @@ module.exports = {
   sendClaimWithoutValueTransfer,
   sendClaimValueTransfer,
   approveUserfeedsContractTokenTransfer,
+  allowanceUserfeedsContractTokenTransfer,
   sendClaimTokenTransfer,
 };
 
