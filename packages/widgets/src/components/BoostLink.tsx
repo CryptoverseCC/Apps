1c0ba8e7011792e1f53cd3ddfed492a0480fc31a
Maciej Górski <maciek.gorski@gmail.com>
2017-09-29 14:28:26 +0200

    Update js-core.

--- packages/widgets/src/components/BoostLink.tsx
+++ packages/widgets/src/components/BoostLink.tsx
@@ -177,7 +177,7 @@ export default class BoostLink extends Component<IBidLinkProps, IBidLinkState> {
     if (typeof token == 'undefined') {
       sendClaimPromise = core.ethereum.claims.sendClaimValueTransfer(web3, recipientAddress, value, claim);
     } else {
-      sendClaimPromise = core.ethereum.erc20.getErc20ContractDecimals(web3, token).then((decimals) => {
+      sendClaimPromise = core.ethereum.erc20.erc20ContractDecimals(web3, token).then((decimals) => {
         const valueAsInt = Math.floor(value * Math.pow(10, decimals));
         return core.ethereum.claims.approveUserfeedsContractTokenTransfer(web3, token, valueAsInt)
           .then((s) => core.ethereum.claims.sendClaimTokenTransfer(web3, recipientAddress, token, valueAsInt, claim));
