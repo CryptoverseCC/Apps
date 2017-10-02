666a2a69377a6699856832bcebd63e8287cd78f0
Jakub Stasiak <kubafr2@gmail.com>
2017-10-02 16:19:05 +0200

    Removed userfeeds-js-core

--- packages/userfeeds-js-core/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "~~~~~Publishing version: $CI_BRANCH~~~~~"
-envsub package.json package.json
-cat package.json
-echo $NPMRC > ~/.npmrc
-npm publish
--- packages/userfeeds-js-core/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "~~~~~Testing version: $CI_BRANCH~~~~~"
-envsub package.json package.json
-cat package.json
-yarn test
