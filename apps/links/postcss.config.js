b71bd0fe0746dd79e34502823c63c8b4d4ae5256
Jakub Stasiak <kubafr2@gmail.com>
2017-10-11 14:26:09 +0200

    [dev] Extracted base webpack config && upgraded some deps

---
+++ apps/links/postcss.config.js
@@ -0,0 +1,6 @@
+const merge = require('webpack-merge');
+
+const baseConfig = require('../../config/postcss.base');
+
+module.exports = merge(baseConfig, {
+});
---
+++ packages/widgets/webpack.config.prod.js
@@ -0,0 +1,6 @@
+const merge = require('webpack-merge');
+
+const devConfig = require('./webpack.config');
+const baseProdConfig = require('../../config/webpack.base.prod');
+
+module.exports = merge(devConfig, baseProdConfig);
