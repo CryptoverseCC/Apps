bd0e4035f325029d77f655c04c3da46a4a9cd63d
Jakub Stasiak <kubafr2@gmail.com>
2017-10-19 11:40:32 +0200

    [widget] web3 on window.load

--- packages/utils/src/web3.ts
+++ packages/utils/src/web3.ts
@@ -2,10 +2,16 @@ import Web3 from 'web3';
 
 const web3 = new Web3();
 
-setTimeout(() => {
+const setProviderIfAvailable = () => {
   if (typeof window.web3 !== 'undefined') {
     web3.setProvider(window.web3.currentProvider);
   }
-});
+};
+
+if (document.readyState === 'complete') {
+  setProviderIfAvailable();
+} else {
+  window.addEventListener('load', setProviderIfAvailable);
+}
 
 export default web3;
