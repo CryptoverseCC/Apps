2b104edbc4abf5f1253535a9ee981982f121311e
Jakub Stasiak <kubafr2@gmail.com>
2017-10-21 18:31:13 +0200

    Added missing reexport to make test successful

--- packages/widgets/src/components/Web3StateProvider.tsx
+++ packages/widgets/src/components/Web3StateProvider.tsx
@@ -3,4 +3,6 @@ import { connect } from 'react-redux';
 import { web3Enabled } from '../selectors/web3';
 import Web3StateProvider from '@userfeeds/apps-components/src/Providers/Web3StateProvider';
 
+export { default as Web3StateProvider } from '@userfeeds/apps-components/src/Providers/Web3StateProvider';
+
 export default connect((state) => ({web3State: web3Enabled(state)}))(Web3StateProvider);
