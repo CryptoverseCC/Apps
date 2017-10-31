6e64a136e9ad51b2a448e4ef0f211eb9a269ef9c
Jakub Stasiak <kubafr2@gmail.com>
2017-10-31 21:43:15 +0100

    ci..

--- apps/links/src/pages/Status/index.tsx
+++ apps/links/src/pages/Status/index.tsx
@@ -166,9 +166,8 @@ export default class Status extends Component<IStatusProps, IStatusState> {
       const rankingApiUrl = `${apiUrl}/ranking/${asset}:${recipientAddress}/${algorithm}/`;
       const allLinksRequest = fetch(rankingApiUrl, { cache: 'no-store' }).then((res) => res.json());
       const whitelistQueryParam = whitelist ? `?whitelist=${whitelist}` : '';
-      const whitelistedLinksRequest = fetch(`${rankingApiUrl}${whitelistQueryParam}`, { cache: 'no-store' }).then((res) =>
-        res.json(),
-      );
+      const whitelistedLinksRequest = fetch(`${rankingApiUrl}${whitelistQueryParam}`, { cache: 'no-store' })
+        .then((res) => res.json());
 
       const [allLinks, whitelistedLinks] = await Promise.all([allLinksRequest, whitelistedLinksRequest]);
       // suboptimal: make a map id to link before mapping links instead
