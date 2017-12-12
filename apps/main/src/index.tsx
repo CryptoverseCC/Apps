31fad384acf9f2c35a9ea1a919d75743e47e12cf
Jakub Stasiak <kubafr2@gmail.com>
2017-12-12 22:01:28 +0100

    Working widget details

--- apps/main/src/index.tsx
+++ apps/main/src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import qs from 'qs';
 
-import web3, { Web3Provider } from '@linkexchange/utils/web3';
+import web3, { Web3Provider, getInfura } from '@linkexchange/utils/web3';
 
 import getStore from './store';
 import App from './App';
@@ -11,11 +11,18 @@ import App from './App';
 import '../styles/all.scss';
 
 const [, searchParams] = document.location.href.split('?');
-const store = getStore(qs.parse(searchParams));
+const parsedParams = qs.parse(searchParams);
+const store = getStore(parsedParams);
+
+let infuraWeb3;
+if (parsedParams.asset) {
+  const [network] = parsedParams.asset.split(':');
+  infuraWeb3 = getInfura(network);
+}
 
 render((
   <Provider store={store}>
-    <Web3Provider injectedWeb3={web3}>
+    <Web3Provider injectedWeb3={web3} infuraWeb3={infuraWeb3}>
       <App />
     </Web3Provider>
   </Provider>),
