6f34ccac28de9179c7e2dd992503af5682daf6d7
Jakub Stasiak <kubafr2@gmail.com>
2018-01-15 08:24:15 +0000

    Added cleaning CDN cache to postpublish hook

---
+++ widgets/linkexchange-link/purgeCDNCach.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl http://purge.jsdelivr.net/npm/@linkexchange/widgets@latest
+curl http://purge.jsdelivr.net/npm/@linkexchange/widgets@latest/build/0.bundle.js
+curl http://purge.jsdelivr.net/npm/@linkexchange/widgets@latest/build/1.bundle.js
+curl http://purge.jsdelivr.net/npm/@linkexchange/widgets@latest/build/2.bundle.js
