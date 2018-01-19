ff05fdabb6752a6c7727abc88d0e1b62f90bd4c5
Jakub Stasiak <kubafr2@gmail.com>
2018-01-19 14:02:38 +0100

    [ci skip] links and simplified build in dockerfile

--- apps/main/src/pages/Video/Configurator/components/Summary.tsx
+++ apps/main/src/pages/Video/Configurator/components/Summary.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import qs from 'qs';
 
 const BASE_EVENT_URL =
-  process.env.NODE_ENV === 'production' ? 'https://linkexchange.io/apps/video' : `${window.location.origin}/video`;
+  process.env.NODE_ENV === 'production' ? 'https://apps.linkexchange.io/video' : `${window.location.origin}/video`;
 
 const Summary = (props) => {
   const eventLink = `${BASE_EVENT_URL}${props.location.search}`;
