8fb53a2be25dfbc1f5b8df189d0ce481e78db82e
Michał Warda <t1du53k@gmail.com>
2018-02-07 14:34:52 +0100

    Bring back specs

--- packages/web3-state-provider/Web3StateProvider.test.tsx
+++ packages/web3-state-provider/Web3StateProvider.test.tsx
@@ -1,14 +1,9 @@
-// import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { Web3StateProvider } from './';
+// import Web3StateProvider from './index';
 
-Enzyme.configure({
-  adapter: new Adapter(),
-});
-
-describe.skip('Web3StateProvider', () => {
+xdescribe('Web3StateProvider', () => {
   it('calls synchronizeState on mount', () => {
     const render = jest.fn();
     const synchronizeState = jest.fn();
