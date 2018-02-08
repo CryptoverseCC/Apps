b6af577dab3ce6c95a2aff8ceec3a177435a1119
Michał Warda <t1du53k@gmail.com>
2018-02-08 11:59:55 +0100

    Add storybook addons

--- packages/new-add-link/index.story.tsx
+++ packages/new-add-link/index.story.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs/react'
+import { action } from '@storybook/addon-actions';
 import Button from './index';
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .add('with text', () => (
-    <Button>Hello Button</Button>
+    <Button onClick={action('button-click')}>{text('Button Text', 'text')}</Button>
   ))
   .add('with some emoji', () => (
     <Button>😀 😎 👍 💯</Button>
--- packages/new-add-link/index.tsx
+++ packages/new-add-link/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = (props) => {
-  return <button>{props.children}</button>;
+  return <button onClick={props.onClick}>{props.children}</button>;
 }
 
 export default Button;
