eb70a2e9c0445d917a29010c2adde1b64ecd82a8
Michał Warda <t1du53k@gmail.com>
2018-02-23 14:23:19 +0100

    further extract add link components

--- apps/storybook/new-add-link.story.tsx
+++ apps/storybook/new-add-link.story.tsx
@@ -2,75 +2,48 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 import { action } from '@storybook/addon-actions';
-
-import { Form, Field, FormSpy } from 'react-final-form';
-
-import { TextField, validateField } from '@linkexchange/components/src/Form/Field';
-import Button from '@linkexchange/components/src/NewButton';
-import { R } from '@linkexchange/utils/validation';
-import { Balance } from '@linkexchange/boost-link/components/Header';
-import * as Modal from '@linkexchange/components/src/StyledComponents';
-import Slide from '@linkexchange/components/src/Slide';
-import TransactionInProgress from '@linkexchange/components/src/TransactionInProgress';
-import Result from '@linkexchange/components/src/Result';
-import AskForAllowance from '@linkexchange/components/src/AskForAllowance';
-import { CopyFromMM } from '@linkexchange/copy-from-mm';
-import AddLink from '@linkexchange/new-add-link/index';
+import Modal from '@linkexchange/components/src/StyledComponents';
+import {
+  AddLinkForm,
+  PaymentInProgress,
+  ConfirmationToUseTokens,
+  ActionRejected,
+  TokensAccess,
+} from '@linkexchange/new-add-link/index';
+import { Provider } from 'mobx-react';
 
 storiesOf('Add Link', module)
   .addDecorator(withKnobs)
+  .addDecorator((storyFn) => (
+    <div style={{ width: '500px' }}>
+      <Modal>{storyFn()}</Modal>
+    </div>
+  ))
   .add('Form', () => {
     const balance = number('balance', 1000);
     const approved = number('approved', 100);
     const minimalValue = number('minimalValue', 0);
-    const currency = text('currency', 'BEN')
-    const submitErrorText = text('Submit Error')
+    const currency = text('currency', 'BEN');
+    const submitErrorText = text('Submit Error');
     const onSubmit = action('Submitted form');
     const formValidations = { title: [], summary: [], target: [], value: [] };
     return (
-      <div style={{ width: '500px' }}>
-        <AddLink
-          balance={balance}
-          currency={currency}
-          approved={approved}
-          onSubmit={onSubmit}
-          submitErrorText={submitErrorText}
-          formValidations={formValidations}
-          minimalValue={minimalValue}
-        />
-      </div>
+      <Provider
+        balance={balance}
+        currency={currency}
+        approved={approved}
+        onSubmit={onSubmit}
+        submitErrorText={submitErrorText}
+        formValidations={formValidations}
+        minimalValue={minimalValue}
+      >
+        <AddLinkForm />
+      </Provider>
     );
   })
-  .add('Payment in progress', () => (
-    <div style={{ width: '500px' }}>
-      <Modal.default centered>
-        <TransactionInProgress>Payment in progress</TransactionInProgress>
-      </Modal.default>
-    </div>
-  ))
-  .add('Confirmation to use tokens', () => (
-    <div style={{ width: '500px' }}>
-      <Modal.default centered>
-        <TransactionInProgress>Receiving confirmation to use your tokens by our contract</TransactionInProgress>
-      </Modal.default>
-    </div>
-  ))
-  .add('Your transaction has been rejected', () => (
-    <div style={{ width: '500px' }}>
-      <Modal.default centered>
-        <Result onClick={action('Retrying!')} type="failure">
-          Your transaction has been rejected.
-        </Result>
-      </Modal.default>
-    </div>
-  ))
+  .add('Payment in progress', () => <PaymentInProgress />)
+  .add('Confirmation to use tokens', () => <ConfirmationToUseTokens />)
+  .add('Your transaction has been rejected', () => <ActionRejected retry={action('Retrying!')} />)
   .add('Tokens access', () => (
-    <div style={{ width: '500px' }}>
-      <Modal.default>
-        <AskForAllowance
-          goBack={action('Go Back')}
-          startTransaction={action('Start transaction')}
-        />
-      </Modal.default>
-    </div>
+    <TokensAccess goBack={action('Go Back')} startTransaction={action('Start transaction')} />
   ));
--- packages/new-add-link/index.tsx
+++ packages/new-add-link/index.tsx
@@ -4,95 +4,124 @@ import { Field, Form } from 'react-final-form';
 import { TextField, validateField } from '@linkexchange/components/src/Form/Field';
 import Button from '@linkexchange/components/src/NewButton';
 import { R } from '@linkexchange/utils/validation';
+import TransactionInProgress from '@linkexchange/components/src/TransactionInProgress';
+import { inject, observer } from 'mobx-react';
+import Result from '@linkexchange/components/src/Result';
+import AskForAllowance from '@linkexchange/components/src/AskForAllowance';
 
-const InUse = ({balance, currency}) => (
-  <Modal.Balance>
-    <Modal.BalanceLabel>In Use:</Modal.BalanceLabel>
-    <Modal.BalanceValue>
-      {balance} {currency}
-    </Modal.BalanceValue>
-  </Modal.Balance>
+const InUse = inject('balance', 'currency')(
+  observer(({ balance, currency }) => (
+    <Modal.Balance>
+      <Modal.BalanceLabel>In Use:</Modal.BalanceLabel>
+      <Modal.BalanceValue>
+        {balance} {currency}
+      </Modal.BalanceValue>
+    </Modal.Balance>
+  )),
 );
 
-const Approved = ({approved, currency}) => (
-  <Modal.Balance>
-    <Modal.BalanceLabel>Approved:</Modal.BalanceLabel>
-    <Modal.BalanceValue>
-      {approved} {currency}
-    </Modal.BalanceValue>
-  </Modal.Balance>
+const Approved = inject('approved', 'currency')(
+  observer(({ approved, currency }) => (
+    <Modal.Balance>
+      <Modal.BalanceLabel>Approved:</Modal.BalanceLabel>
+      <Modal.BalanceValue>
+        {approved} {currency}
+      </Modal.BalanceValue>
+    </Modal.Balance>
+  )),
 );
 
-const AddLink = ({ balance, currency, approved, onSubmit, submitErrorText, formValidations, minimalValue }) => (
-  <Modal.default>
-    <Modal.Header>
-      <Modal.Title>Create a new link</Modal.Title>
-      <Modal.HeaderRight>
-        <InUse balance={balance} currency={currency} />
-        <Approved approved={approved} currency={currency} />
-      </Modal.HeaderRight>
-    </Modal.Header>
-    <Form
-      initialValues={{
-        target: 'http://',
-        value: '0' || '',
-      }}
-      onSubmit={onSubmit}
-    >
-      {({ handleSubmit, pristine, invalid, submitError }) => (
-        <React.Fragment>
-          {submitError || (submitErrorText && <Modal.Error>{submitError || submitErrorText}</Modal.Error>)}
-          <Modal.Body>
-            <form onSubmit={handleSubmit}>
-              <Field
-                title="Headline"
-                component={TextField}
-                name="title"
-                validate={validateField([R.required, R.maxLength(35), ...(formValidations.title || [])])}
-              />
-              <Field
-                multiline
-                title="Description"
-                component={TextField}
-                name="summary"
-                validate={validateField([R.required, R.maxLength(70), ...(formValidations.summary || [])])}
-              />
-              <Field
-                title="Link"
-                component={TextField}
-                name="target"
-                validate={validateField([R.required, R.link, ...(formValidations.target || [])])}
-              />
-              <Field
-                title="Initial Fee"
-                component={TextField}
-                name="value"
-                bold
-                currency={currency}
-                validate={validateField([
-                  R.required,
-                  R.number,
-                  R.currencyDecimals(4),
-                  R.greaterThan(minimalValue ? parseInt(minimalValue, 10) : 0),
-                  R.lessThen(balance),
-                  ...(formValidations.value || []),
-                ])}
-              />
-              <Button size="big" type="submit" color="primary" style={{ width: '100%', marginTop: '40px' }}>
-                Send
-              </Button>
-            </form>
-          </Modal.Body>
-        </React.Fragment>
-      )}
-    </Form>
-    <Modal.Footer>
-      <Modal.FooterText>
-        You need to pay initial fee to be considered. Paying fee does not imply that your link will be displayed, as
-        publisher has to whitelist your link.
-      </Modal.FooterText>
-    </Modal.Footer>
-  </Modal.default>
+export const PaymentInProgress = () => <TransactionInProgress>Payment in progress</TransactionInProgress>;
+
+export const ConfirmationToUseTokens = () => (
+  <TransactionInProgress>Receiving confirmation to use your tokens by our contract</TransactionInProgress>
 );
 
-export default AddLink;
+export const ActionRejected = ({ retry }) => (
+  <Result onClick={retry} type="failure">
+    Your transaction has been rejected.
+  </Result>
+);
+
+export const TokensAccess = AskForAllowance;
+
+export const AddLinkForm = inject(
+  'balance',
+  'currency',
+  'onSubmit',
+  'submitErrorText',
+  'formValidations',
+  'minimalValue',
+)(
+  observer(({ balance, currency, onSubmit, submitErrorText, formValidations, minimalValue }) => (
+    <React.Fragment>
+      <Modal.Header>
+        <Modal.Title>Create a new link</Modal.Title>
+        <Modal.HeaderRight>
+          <InUse />
+          <Approved />
+        </Modal.HeaderRight>
+      </Modal.Header>
+      <Form
+        initialValues={{
+          target: 'http://',
+          value: '0' || '',
+        }}
+        onSubmit={onSubmit}
+      >
+        {({ handleSubmit, pristine, invalid, submitError }) => (
+          <React.Fragment>
+            {(submitError || submitErrorText) && <Modal.Error>{submitError || submitErrorText}</Modal.Error>}
+            <Modal.Body>
+              <form onSubmit={handleSubmit}>
+                <Field
+                  title="Headline"
+                  component={TextField}
+                  name="title"
+                  validate={validateField([R.required, R.maxLength(35), ...(formValidations.title || [])])}
+                />
+                <Field
+                  multiline
+                  title="Description"
+                  component={TextField}
+                  name="summary"
+                  validate={validateField([R.required, R.maxLength(70), ...(formValidations.summary || [])])}
+                />
+                <Field
+                  title="Link"
+                  component={TextField}
+                  name="target"
+                  validate={validateField([R.required, R.link, ...(formValidations.target || [])])}
+                />
+                <Field
+                  title="Initial Fee"
+                  component={TextField}
+                  name="value"
+                  bold
+                  currency={currency}
+                  validate={validateField([
+                    R.required,
+                    R.number,
+                    R.currencyDecimals(4),
+                    R.greaterThan(minimalValue ? parseInt(minimalValue, 10) : 0),
+                    R.lessThen(balance),
+                    ...(formValidations.value || []),
+                  ])}
+                />
+                <Button size="big" type="submit" color="primary" style={{ width: '100%', marginTop: '40px' }}>
+                  Send
+                </Button>
+              </form>
+            </Modal.Body>
+          </React.Fragment>
+        )}
+      </Form>
+      <Modal.Footer>
+        <Modal.FooterText>
+          You need to pay initial fee to be considered. Paying fee does not imply that your link will be displayed, as
+          publisher has to whitelist your link.
+        </Modal.FooterText>
+      </Modal.Footer>
+    </React.Fragment>
+  )),
+);
