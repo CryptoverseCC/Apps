f51a27187ddf9140ec12114fa5f16802cf76f429
Michał Warda <t1du53k@gmail.com>
2018-02-23 12:00:35 +0100

    extract add link components

--- apps/storybook/.storybook/config.js
+++ apps/storybook/.storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
 
 const req = require.context('../', true, /\.story\.tsx$/)
-console.log(req);
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
--- packages/new-add-link/index.tsx
+++ packages/new-add-link/index.tsx
@@ -5,23 +5,31 @@ import { TextField, validateField } from '@linkexchange/components/src/Form/Fiel
 import Button from '@linkexchange/components/src/NewButton';
 import { R } from '@linkexchange/utils/validation';
 
-const AddLink = ({balance, currency, approved, onSubmit, submitErrorText, formValidations, minimalValue}) => (
+const InUse = ({balance, currency}) => (
+  <Modal.Balance>
+    <Modal.BalanceLabel>In Use:</Modal.BalanceLabel>
+    <Modal.BalanceValue>
+      {balance} {currency}
+    </Modal.BalanceValue>
+  </Modal.Balance>
+);
+
+const Approved = ({approved, currency}) => (
+  <Modal.Balance>
+    <Modal.BalanceLabel>Approved:</Modal.BalanceLabel>
+    <Modal.BalanceValue>
+      {approved} {currency}
+    </Modal.BalanceValue>
+  </Modal.Balance>
+);
+
+const AddLink = ({ balance, currency, approved, onSubmit, submitErrorText, formValidations, minimalValue }) => (
   <Modal.default>
     <Modal.Header>
       <Modal.Title>Create a new link</Modal.Title>
       <Modal.HeaderRight>
-        <Modal.Balance>
-          <Modal.BalanceLabel>In Use:</Modal.BalanceLabel>
-          <Modal.BalanceValue>
-            {balance} {currency}
-          </Modal.BalanceValue>
-        </Modal.Balance>
-        <Modal.Balance>
-          <Modal.BalanceLabel>Approved:</Modal.BalanceLabel>
-          <Modal.BalanceValue>
-            {approved} {currency}
-          </Modal.BalanceValue>
-        </Modal.Balance>
+        <InUse balance={balance} currency={currency} />
+        <Approved approved={approved} currency={currency} />
       </Modal.HeaderRight>
     </Modal.Header>
     <Form
