00e2e6555ba197938a054188badf94ac26164282
Michał Warda <michal.warda@elpassion.pl>
2018-02-27 15:39:27 +0100

    Development: Add periodical injectedWeb3State updating.

--- packages/web3-store/index.test.tsx
+++ packages/web3-store/index.test.tsx
@@ -1,22 +1,41 @@
 import { observable, extendObservable, computed, observe, action } from 'mobx';
 import { networkMapping } from '@userfeeds/core/src/utils';
 class Web3Store {
+  stopUpdatingInjectedWeb3State: any;
+
+  currentProvider: any;
+  isListening: boolean;
+  injectedWeb3ActiveNetwork: string;
+
   asset: string;
-  @observable activeNetwork: string = undefined;
+
   constructor(private injectedWeb3, initialState) {
     extendObservable(this, initialState);
+    this.startUpdatingInjectedWeb3State();
+  }
+
+  startUpdatingInjectedWeb3State() {
     this.updateInjectedWeb3State();
+    this.stopUpdatingInjectedWeb3State = setInterval(this.updateInjectedWeb3State, 1000);
   }
 
-  @action
+  @action.bound
   async updateInjectedWeb3State() {
-    this.activeNetwork = networkMapping[await this.injectedWeb3.eth.net.getId()];
+    this.currentProvider = this.injectedWeb3.currentProvider;
+    this.isListening = await this.injectedWeb3.eth.net.isListening();
+    const networkId = await this.injectedWeb3.eth.net.getId();
+    this.injectedWeb3ActiveNetwork = networkMapping[networkId];
   }
 
-  ready = () =>
-    new Promise((resolve) => {
-      resolve();
-    });
+  @computed
+  get activeNetwork() {
+    return this.ready ? this.injectedWeb3ActiveNetwork : undefined;
+  }
+
+  @computed
+  get ready() {
+    return !!(this.currentProvider && this.isListening);
+  }
 
   @computed
   get network() {
@@ -30,42 +49,94 @@ class Web3Store {
 }
 
 describe('Web3Store', () => {
+  const isListening = jest.fn().mockReturnValue(Promise.resolve(true));
+  const getId = jest.fn().mockReturnValue(Promise.resolve(1));
   const injectedWeb3 = {
     eth: {
       net: {
-        isListening: jest.fn().mockReturnValue(Promise.resolve(true)),
-        getId: jest.fn().mockReturnValue(Promise.resolve(1)),
+        isListening,
+        getId,
       },
     },
-    currentProvider: null,
+    currentProvider: true,
   };
+
   test('sets initial asset', () => {
     const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum' });
     expect(web3Store.asset).toBe('ethereum');
   });
+
   test('computes ethereum network', () => {
     const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum' });
     expect(web3Store.network).toBe('ethereum');
   });
+
   test('computes other network', () => {
     const web3Store = new Web3Store(injectedWeb3, { asset: 'rinkeby' });
     expect(web3Store.network).toBe('rinkeby');
   });
+
   test('computes network when asset is token', () => {
     const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum:0x0' });
     expect(web3Store.network).toBe('ethereum');
   });
+
   test('computes token when asset is token', () => {
     const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum:0x0' });
     expect(web3Store.token).toBe('0x0');
   });
+
   test('sets token to undefined when asset is not token', () => {
     const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum' });
     expect(web3Store.token).toBe(undefined);
   });
-  test('exposes activeNetwork', async () => {
-    const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum' });
-    await web3Store.ready();
+
+  test('activeNetwork is undefined when currentProvider is false', () => {
+    const web3Store = new Web3Store(injectedWeb3, {
+      asset: 'ethereum',
+      currentProvider: false,
+    });
+    expect(web3Store.activeNetwork).toBe(undefined);
+  });
+
+  test('activeNetwork is undefined when currentProvider is true but is not listening', () => {
+    const web3Store = new Web3Store(injectedWeb3, {
+      asset: 'ethereum',
+      currentProvider: true,
+      isListening: false,
+    });
+    expect(web3Store.activeNetwork).toBe(undefined);
+  });
+
+  test('activeNetwork is correct when currentProvider is true and is listening', () => {
+    const web3Store = new Web3Store(injectedWeb3, {
+      asset: 'ethereum',
+      currentProvider: true,
+      isListening: true,
+      injectedWeb3ActiveNetwork: 'ethereum',
+    });
     expect(web3Store.activeNetwork).toBe('ethereum');
+  });
+
+  test('#updateInjectedWeb3State correctly updates state', async () => {
+    const web3Store = new Web3Store(injectedWeb3, { asset: 'ethereum' });
+    injectedWeb3.currentProvider = false;
+    injectedWeb3.eth.net.isListening.mockReturnValueOnce(Promise.resolve(false));
+    injectedWeb3.eth.net.getId.mockReturnValueOnce(Promise.resolve(1)).mockReturnValueOnce(Promise.resolve(2));
+    await web3Store.updateInjectedWeb3State();
+    expect(web3Store.currentProvider).toBe(false);
+    expect(web3Store.isListening).toBe(false);
+    expect(web3Store.injectedWeb3ActiveNetwork).toBe(undefined);
+  });
+
+  test('updates data from injectedWeb3 every second', () => {
+    jest.useFakeTimers();
+    const web3Store = new Web3Store(injectedWeb3, {
+      asset: 'ethereum',
+      currentProvider: true,
+      isListening: true,
+      injectedWeb3ActiveNetwork: 'ethereum',
+    });
+    expect(setInterval).toHaveBeenCalledWith(web3Store.updateInjectedWeb3State, 1000);
   });
 });
